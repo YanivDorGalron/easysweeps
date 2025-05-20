9070290acae48aec008348b32c8e3c043153e5f5
Yaniv Galron <yanivdorgalron@gmail.com>
2025-05-20 21:19:52 +0300

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="easysweeps",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         "click>=8.0.0",
